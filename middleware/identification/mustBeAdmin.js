f1ae1e9a240ccfa001591fc42ff232f62e23b9e6
lucieHenallux <resimontlucie@gmail.com>
2025-11-17 09:38:31 +0100

    solved merge markers

--- middleware/identification/mustBeAdmin.js
+++ middleware/identification/mustBeAdmin.js
@@ -13,16 +13,11 @@
 
 export const mustBeAdmin = (req, res, next) => {
     try {
-<<<<<<< HEAD
-        
-        const isAdmin = req.user.is_admin; 
-=======
         if (!req.user) {
             return res.status(401).send("Authentication required.");
         }
 
         const isAdmin = req.user.isAdmin; 
->>>>>>> 326b842274a3f2c8f7cdbfdec6f39b825b064bfe
         if (isAdmin === true) {
             return next();
         } else {
@@ -30,11 +25,6 @@ export const mustBeAdmin = (req, res, next) => {
         }
 
     } catch (err) {
-<<<<<<< HEAD
-
-        res.status(500).send("Internal Server Error.");
-=======
         next("error");
->>>>>>> 326b842274a3f2c8f7cdbfdec6f39b825b064bfe
     }
 };
